8dadc20afca6db416b0b162e8bd4e53d43f1477e
asaprolfy <rolf@mio-chan-is.best>
2024-12-15 21:42:25 -0600

    Changed to have abs conf file path

--- default_start_nrue.sh
+++ default_start_nrue.sh
@@ -23,14 +23,19 @@ elif [[ -d "$log_file" ]]; then
   rm -r "$log_file"
 fi
 
+if [[ -z "$2" ]]; then
+  nrue_conf_file="/root/openairinterface5g/targets/PROJECTS/GENERIC-NR-5GC/CONF/ue.conf"
+else
+  nrue_conf_file="$2"
+fi
+
 uhd_find_devices
 
 cd /root/openairinterface5g || exit
 source oaienv
 cd cmake_targets/ran_build/build || exit
 
-./nr-uesoftmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/ue.conf \
-                 -r 106 --numerology 1 --band 78 -C 3604800000 --ue-fo-compensation \
+./nr-uesoftmodem -O "$nrue_conf_file" -r 106 --numerology 1 --band 78 -C 3604800000 --ue-fo-compensation \
                  --sa -E --ue-txgain 0 --usrp-args "serial=8000170" --nokrnmod 1 \
                  > "$log_file" &
 
